8e30a4c258d44fe476b10cfe5c26d385fc95c93b
MrKubis <jakub.f102@gmail.com>
2025-09-03 18:16:02 +0200

    init:Created a Weather Model

--- backend/django/nubiweather/api/models.py
+++ backend/django/nubiweather/api/models.py
@@ -1,3 +1,24 @@
 from django.db import models
-
+from django.utils import timezone
+import uuid
 # Create your models here.
+
+class Weather(models.Model):
+    id = models.UUIDField(
+        default=uuid.uuid4,
+        unique= True,
+        primary_key = True,
+        editable=False
+        )
+    icon = models.CharField(max_length=255,default='')
+    temp_c = models.DecimalField(max_digits=5,decimal_places=2,default=00.00)
+    description = models.CharField(max_length=255)
+    created_at = models.DateTimeField(default=timezone.now)
+    #To wklepuje tutaj bo czasem bez tego nie działa xddd
+    def save(self, **kwargs):
+        super().save(**kwargs)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    def __str__(self):
+        return super().__str__()
+    
