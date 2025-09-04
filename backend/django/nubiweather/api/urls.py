00dcb46a71414c9d8236d405886d8410dc0b8d5b
MrKubis <jakub.f102@gmail.com>
2025-09-04 14:07:13 +0200

    fix:Changed urls
    Now to only get data, use method GET, but to Save data use method POST, and in body type varaibles like this:
    {
    "city" : "Gliwice",
    "days"4
    }

--- backend/django/nubiweather/api/urls.py
+++ backend/django/nubiweather/api/urls.py
@@ -3,7 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path("realtime-weather/",views.getRealtimeWeather),
-    path("realtime-weather/save/",views.saveRealtimeWeather),
     path("forecast-weather/",views.getForecastWeather),
-    path("forecast-weather/save/",views.saveForecastWeather),
 ]
